4dc7506a67bb5d1ac2c2d43bc718a59cb2cdc513
Renaud Loiseleux <renaud.loiseleux@gmail.com>
2019-10-16 21:48:03 +0200

    working v0

--- index.py
+++ index.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, Response, jsonify
 from werkzeug.contrib.fixers import ProxyFix
 import uuid
-from kernel import TheHouse
+from kernel import TheHouse, get_ts
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -13,11 +13,14 @@ def get_uuid():
 
 def messages_stream(room_id):
     listenner_uuid = get_uuid()
+    last_date = get_ts()
     while True:
-        TheHouse.lock.aquire(listenner_uuid)
-        message = TheHouse.read_from_room(room_id)
+        TheHouse.lock.acquire(listenner_uuid)
+        date, message = TheHouse.read_from_room(room_id)
         TheHouse.lock.release()
-        yield "data:{}\n\n".format(messages)
+        if last_date < date:
+            last_date = date
+            yield "data:{}\n\n".format(message)
 
 @app.route("/listen/<room_id>")
 def listner(room_id):
@@ -25,7 +28,7 @@ def listner(room_id):
 
 @app.route("/write/<room_id>", methods=["POST"])
 def write(room_id):
-    TheHouse.write_in_room(room_id, request.data)
+    TheHouse.write_in_room(room_id, request.data.decode("utf8"))
     return jsonify({"success":True})
 
 @app.route("/ping")
@@ -37,3 +40,6 @@ def show_thehouse():
     return jsonify({
         "TheHouse": TheHouse.to_json()
     })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
--- kernel.py
+++ kernel.py
@@ -6,21 +6,28 @@ from queue import Queue
 
 class HouseLock(object):
     def __init__(self):
-        self._lock = Lock()
+        self._master_lock = Lock()
         self._queue = Queue()
 
-    def aquire(self, listenner):
-        if self._lock.locked():
-            self._queue.put(listenner)
-        return self._lock.aquire()
+    def acquire(self, listenner):
+        if not self._master_lock.locked():
+            self._master_lock.acquire()
+            return
+        listenner_lock = Lock()
+        listenner_lock.acquire()
+        self._queue.put(listenner_lock)
+        return listenner_lock.acquire()
 
     def release(self):
-        self._lock.release()
+        if self._queue.empty():
+            return self._master_lock.release()
+        next_lock = self._queue.get()
+        return next_lock.release()
 
 class TheHouse(object):
     _rooms = {}
     lock = HouseLock()
-    lock.aquire("TheRoom")
+    lock.acquire("TheRoom")
 
     @staticmethod
     def _get_room(id):
@@ -31,11 +38,18 @@ class TheHouse(object):
         return room
 
     @staticmethod
-    def write_in_room(id, message):
+    def write_in_room(room_id, message):
         TheHouse._get_room(room_id)._write(message)
         TheHouse.lock.release()
         # we let through every listener in the queue for The House
-        TheHouse.lock.aquire("TheRoom")
+        TheHouse.lock.acquire("TheRoom")
+
+    @staticmethod
+    def read_from_room(room_id):
+        room = TheHouse._rooms.get(room_id)
+        if room is None:
+            return 0, None
+        return room.last_update, room.last_message
 
     @staticmethod
     def to_json():
@@ -48,11 +62,14 @@ class Room(object):
         self.last_message = None
 
     def _update(self):
-        self.last_update = int(datetime.now().timestamp() * 1000)
+        self.last_update = get_ts()
 
     def _write(self, message):
         self.last_message = message
         self._update()
 
-    def to_json():
-        return self.__dict__
+    def to_json(self):
+        return vars(self)
+
+def get_ts():
+    return int(datetime.now().timestamp() * 1000)
